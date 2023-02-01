3fbbb980f2518e29340f0ed6a30397a45c815fcd
Anilthapa4 <107929147+Anilthapa4@users.noreply.github.com>
2023-02-02 01:41:13 +0545

    challange 9 done

--- app.js
+++ app.js
@@ -26,6 +26,10 @@ app.get("/contact",function(req,res){
 app.get("/compose",function(req,res){
   res.render("compose");
 });
+app.post("/",function(req,res){
+  let newContent=req.body.compose;
+  console.log(newContent);
+});
 
 
 
