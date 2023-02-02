d703ec126512942d0e03e2e303b2e61435866154
Anilthapa4 <107929147+Anilthapa4@users.noreply.github.com>
2023-02-02 18:47:52 +0545

    challange 18

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import ejs from "ejs";
+import _ from "lodash";//challange 18
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -32,7 +33,7 @@ app.get("/contact", function (req, res) {
 app.get("/compose", function (req, res) {
   res.render("compose");
 });
-app.post("/compose", function (req, res) {
+app.post("/", function (req, res) {
   const post = {
     title: req.body.postTitle,
     content: req.body.postBody
@@ -43,11 +44,16 @@ app.post("/compose", function (req, res) {
   res.redirect("/");
 
 });
+
+//challange 16/17
 app.get("/post/:topic", function (req, res) {
-  const requestedTitle = req.params.topic;
+
+  const requestedTitle = _.lowerCase(req.params.topic);//using lodash for chllge 18
+
   //this is a routing paramters where we can use routing technique to redirect page to various path using a common name which is specified as :topic.
   posts.forEach(function (post) {
-    const storedTitle = post.title;
+
+    const storedTitle = _.lowerCase(post.title);
 
     if (requestedTitle === storedTitle) {
       console.log("Match Found!");
