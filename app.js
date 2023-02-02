93abf6bb9db1d870559f26472ffbc154dc3f4251
Anilthapa4 <107929147+Anilthapa4@users.noreply.github.com>
2023-02-02 23:30:17 +0545

    completed

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ import bodyParser from "body-parser";
 import ejs from "ejs";
 import _ from "lodash";//challange 18
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent = "We Welcome You everyone to the daily journal created by chillflat.Here we post some of the article written by us and our friends.Hope you will go through it once and please let us known if you have anything like this so that we will post to it too.Thank You and enjoy the article.";
+const aboutContent = "Welcome To Our Daily Journal by Chillflats.We Are Chillflatians and this is our daily journal."
+const contactContent = "If You Have any Queries Let us know to earliest.Sorry For the Convenience caused.";
 
 const app = express();
 
@@ -36,7 +36,8 @@ app.get("/compose", function (req, res) {
 app.post("/", function (req, res) {
   const post = {
     title: req.body.postTitle,
-    content: req.body.postBody
+    content: req.body.postBody,
+    author: req.body.postAuthor
   };
 
   posts.push(post);
@@ -56,11 +57,14 @@ app.get("/post/:topic", function (req, res) {
     const storedTitle = _.lowerCase(post.title);
 
     if (requestedTitle === storedTitle) {
-      console.log("Match Found!");
-    };
+        res.render("post",{
+          title:post.title,
+          content:post.content,  
+          author:post.auhtor  //challange 19
+        });
+      };
+    });
   });
-});
-
 
 
 
